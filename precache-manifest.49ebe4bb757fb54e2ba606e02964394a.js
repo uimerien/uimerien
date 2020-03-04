823ef8161e88652f7324eaf4ff4d4794cf14de4b
Rodrigo Godoy MacBook <rodrigocgodoy@hotmail.com>
2020-03-04 00:17:02 -0300

    Updates

---
+++ precache-manifest.49ebe4bb757fb54e2ba606e02964394a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929122e5b6d6a5c9cbbd67f5e75f74d3",
+    "url": "/uimerien/index.html"
+  },
+  {
+    "revision": "5ca48f42a1e3c6ee778f",
+    "url": "/uimerien/static/js/2.e99dbc16.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/uimerien/static/js/2.e99dbc16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582d6828157e914c042d",
+    "url": "/uimerien/static/js/main.bad2a82b.chunk.js"
+  },
+  {
+    "revision": "1f4efdc0b01f1e4e56be",
+    "url": "/uimerien/static/js/runtime-main.87e96cb1.js"
+  },
+  {
+    "revision": "a5ea6317caef4ad7da8833a3ce7a115b",
+    "url": "/uimerien/static/media/perfil.a5ea6317.jpeg"
+  }
+]);
